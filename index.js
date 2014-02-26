7657291a8bf9653ed0a6edb4518f45b48fa2cf53
Jozef Izso <jozef.izso@gmail.com>
2014-02-26 13:12:02 +0100

    Error document will be loaded from a specified folder.

--- index.js
+++ index.js
@@ -1,5 +1,32 @@
 'use strict';
 
-module.exports = function () {
+var fs = require('fs');
 
+module.exports = function errorDocument(options) {
+    options = options || {};
+
+    // Accept root dir
+    if ('string' === typeof options) {
+        options = { root: options };
+    }
+    if (!options.root) {
+        options.root = 'public';
+    }
+
+    return function errorDocument(err, req, res, next) {
+        if (err.status) {
+            res.statusCode = err.status;
+        }
+
+        var path = options.root + '/' + res.statusCode + '.html';
+
+        fs.readFile(path, 'utf8', function (err, str) {
+            if (err) {
+                return next(err);
+            }
+
+            res.setHeader('Content-Type', 'text/html; charset=utf-8');
+            res.end(str);
+        });
+    };
 };
--- test/test-require.js
+++ test/test-require.js
@@ -5,15 +5,15 @@ var assert = require('chai').assert;
 
 describe('connect-error-document module', function () {
     it('can be imported without blowing up', function () {
-        var app = require('..');
+        var module = require('..');
 
-        assert.isDefined(app, 'connect-error-document module was not required successfully');
-        assert.isFunction(app, 'connect-error-document module is not a function');
+        assert.isDefined(module, 'connect-error-document module was not required successfully');
+        assert.isFunction(module, 'connect-error-document module is not a function');
     });
 
     it('is a function', function () {
-        var app = require('..');
+        var module = require('..');
 
-        assert.isFunction(app, 'connect-error-document module is not a function');
+        assert.isFunction(module, 'connect-error-document module is not a function');
     });
 });
